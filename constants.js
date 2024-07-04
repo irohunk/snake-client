57c2060571f75deaf3fc57e1e9dae4ae10748bc2
Rohan Kulkarni <irohunk@gmail.com>
2024-07-03 22:10:52 -0400

    created constants.js and added keys to it. refactored the code accordingly in other files

---
+++ constants.js
@@ -0,0 +1,33 @@
+const IP = "localhost"; // IP address here,
+const PORT = 50541; // PORT number here,
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+const SAY_NICE_KEY = 'n';
+const SAY_HAHA_KEY = 'h';
+const SAY_GOODGAME_KEY = 'g';
+
+// const key = {
+//   "\u0003": process.exit(),
+//   'w': "Move: up",
+//   'a': "Move: left",
+//   's': "Move: down",
+//   'd': "Move: right",
+//   'n': "Say: nice",
+//   'h': "Say: haha",
+//   'g': "Say: good game"
+// }
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  SAY_GOODGAME_KEY,
+  SAY_HAHA_KEY,
+  SAY_NICE_KEY
+  // key
+};
--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, SAY_GOODGAME_KEY, SAY_HAHA_KEY, SAY_NICE_KEY } = require("./constants")
+
 const setupInput = function (conn) {
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
@@ -11,21 +13,43 @@ const handleUserInput = (key, conn) => {
   if (key === "\u0003") {
     process.exit();  
   }
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     conn.write("Move: up");
-  } else if (key === 'a') {
+  } else if (key === MOVE_LEFT_KEY) {
     conn.write("Move: left");
-  } else if (key === 's') {
+  } else if (key === MOVE_DOWN_KEY) {
     conn.write("Move: down");
-  } else if (key === 'd') {
+  } else if (key === MOVE_RIGHT_KEY) {
     conn.write("Move: right");
-  } else if (key === 'n') {
+  } else if (key === SAY_NICE_KEY) {
     conn.write("Say: nice");
-  } else if (key === 'h') {
+  } else if (key === SAY_HAHA_KEY) {
     conn.write("Say: haha");
-  } else if (key === 'g') {
+  } else if (key === SAY_GOODGAME_KEY) {
     conn.write("Say: good game");
   }
 };
+
+
+// const handleUserInput = (key, conn) => {
+//   if (key === "\u0003") {
+//     process.exit();  
+//   }
+//   if (key === 'w') {
+//     conn.write("Move: up");
+//   } else if (key === 'a') {
+//     conn.write("Move: left");
+//   } else if (key === 's') {
+//     conn.write("Move: down");
+//   } else if (key === 'd') {
+//     conn.write("Move: right");
+//   } else if (key === 'n') {
+//     conn.write("Say: nice");
+//   } else if (key === 'h') {
+//     conn.write("Say: haha");
+//   } else if (key === 'g') {
+//     conn.write("Say: good game");
+//   }
+// };
 
 module.exports = setupInput;
